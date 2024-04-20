a233a0c3e441904bb3fa245b40fd3ebe85b7748a
Justin Pearson <justinpearson@users.noreply.github.com>
2024-04-19 22:09:09 -0700

    jpp - magic tree uses currently selected color, instead of hard-coded to green.

--- js/tools/trees.js
+++ js/tools/trees.js
@@ -20,6 +20,17 @@ KiddoPaint.Tools.Toolbox.Tree = function() {
 };
 KiddoPaint.Tools.Tree = new KiddoPaint.Tools.Toolbox.Tree();
 
+function hexToRgb(hex) {
+    // Parse a string like "#22FFFF" into red, green, blue components, each from 0 to 255.
+    // Remove the leading '#' if it exists
+    hex = hex.replace(/^#/, '');
+    // Parse the red, green, and blue components
+    const r = parseInt(hex.substring(0, 2), 16);
+    const g = parseInt(hex.substring(2, 4), 16);
+    const b = parseInt(hex.substring(4, 6), 16);
+    return {r,g,b};
+}
+
 /*
 drawTree(320, 600, 60, -Math.PI / 2, 12, 15);
 drawTree(500, 600, 60, -Math.PI / 2, 12, 7);
@@ -41,24 +52,68 @@ function drawTree(startX, startY, length, angle, depth, branchWidth) {
     KiddoPaint.Display.context.lineWidth = branchWidth;
     KiddoPaint.Display.context.lineTo(endX, endY);
 
-    // tweak color slightly
-    const colorOffset = depth <= 2 ? 128 : 64;
-    const randColor = (((Math.random() * 64) + colorOffset) >> 0);
-    const red = randColor;
-    const green = 0;
-    const blue = 0;
-    KiddoPaint.Display.context.strokeStyle = `rgb(${red},${green},${blue})`;
+    // original:
+    // tweak color slightly based on trunk vs branches of tree.
+    // [red,0,0] where:
+    // red = 64 + [0,64] for trunk
+    // red = 128 + [0,64] for branches
+    // const colorOffset = depth <= 2 ? 128 : 64; // leaves (depth<=2) are brighter (higher offset) than trunk
+    // const randColor = ((Math.random() * 64) + colorOffset) >> 0; // Math.random() in (0,1), so round to int with >>0
+    // const red = randColor;
+    // const green = 0;
+    // const blue = 0;
+    // KiddoPaint.Display.context.strokeStyle = `rgb(${red},${green},${blue})`;
 
-    // old:
-    // if (depth <= 2) {
-    //     // KiddoPaint.Display.context.strokeStyle = 'rgb(' + '0,0,' + (((Math.random() * 64) + 128) >> 0) + ')'; // blue
-    //     KiddoPaint.Display.context.strokeStyle = 'rgb(' + (((Math.random() * 64) + 128) >> 0) + ',0,0)'; // red
-    //     // KiddoPaint.Display.context.strokeStyle = 'rgb(0,' + (((Math.random() * 64) + 128) >> 0) + ',0)'; // green
-    // } else {
-    //     // KiddoPaint.Display.context.strokeStyle = 'rgb(' + '0,20,' + (((Math.random() * 64) + 64) >> 0) + ')'; // blue
-    //     KiddoPaint.Display.context.strokeStyle = 'rgb(' + (((Math.random() * 64) + 64) >> 0) + ',0,20)'; // red
-    //     // KiddoPaint.Display.context.strokeStyle = 'rgb(0,' + (((Math.random() * 64) + 64) >> 0) + ',20)'; // greeen
-    // }
+    // Idea 1: Make tree cur color (no shading)
+    // KiddoPaint.Display.context.strokeStyle = KiddoPaint.Current.color;
+
+    // Idea 2: random branch colors:
+    // const red = Math.random() * 256 >> 0;
+    // const green = Math.random() * 256 >> 0;
+    // const blue = Math.random() * 256 >> 0;
+    // KiddoPaint.Display.context.strokeStyle = `rgb(${red},${green},${blue})`;
+
+    // Idea 3: trunk is randomly darker than selected color, branches are randomly lighter.
+    //
+    const rgbStr = KiddoPaint.Current.color; // eg "rgb(254, 231, 97)"
+    // console.log(`Cur color: ${rgbStr}`)
+    let newColor = rgbStr;
+
+    const rgb = (() => {
+        const matches = rgbStr.match(/rgb\((\d+), ?(\d+), ?(\d+)\)/);
+        if (matches) {
+            return {
+                r: parseInt(matches[1]),
+                g: parseInt(matches[2]),
+                b: parseInt(matches[3])
+            };
+        }
+
+        if (rgbStr.startsWith('#')) {
+            return hexToRgb(rgbStr);
+        }
+
+        // console.log(`Unexpected color format (expected string "rgb(123,234,111)"): ${rgbStr}`);
+        // debugger;
+        return undefined;
+    })();
+
+    if (rgb) {
+        const hsl = rgbToHsl(rgb.r, rgb.g, rgb.b);
+        // brighten (inc lightness) of leaves (depth<=2)
+        // darken (dec lightness) of trunk (depth>2)
+        const newLightness = clamp(hsl.l + (depth <= 2 ? 1 : -1) * Math.random() * 0.25, 0, 1);
+        const hsl2 = {
+            h: hsl.h,
+            s: hsl.s,
+            l: newLightness
+        };
+        const rgb2 = hslToRgb(hsl2.h, hsl2.s, hsl2.l);
+        newColor = `rgb(${rgb2.r},${rgb2.g},${rgb2.b})`;
+        // console.log('potato', newColor);
+    }
+    KiddoPaint.Display.context.strokeStyle = newColor;
+
 
     KiddoPaint.Display.context.stroke();
     newDepth = depth - 1;
